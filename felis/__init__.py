4fa6a5c4406947e2ec249613b7cb7548bf112f8c
LeeeeT <me@LeeeeT.dev>
2024-04-19 19:38:31 +0300

    Export `felis.io.IO` in `felis.__init__`

--- felis/__init__.py
+++ felis/__init__.py
@@ -1,11 +1,12 @@
 from felis.coroutine import Coroutine
 from felis.either import Either
+from felis.io import IO
 from felis.lazy import Lazy
 from felis.list import List
 from felis.option import Option
 from felis.reverse_state import ReverseState
 from felis.state import State
 
-__all__ = ["Coroutine", "Either", "Lazy", "List", "Option", "ReverseState", "State"]
+__all__ = ["Coroutine", "Either", "IO", "Lazy", "List", "Option", "ReverseState", "State"]
 
 __version__ = "0.dev0"
