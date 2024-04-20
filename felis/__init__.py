1aeb171d4cd175c22b1ff0dea1e1f1bd27e62823
LeeeeT <me@leeeet.dev>
2024-04-20 16:45:33 +0300

    Add `felis.environment` monad

--- felis/__init__.py
+++ felis/__init__.py
@@ -1,5 +1,6 @@
 from felis.coroutine import Coroutine
 from felis.either import Either
+from felis.environment import Environment
 from felis.io import IO
 from felis.lazy import Lazy
 from felis.list import List
@@ -7,6 +8,6 @@ from felis.option import Option
 from felis.reverse_state import ReverseState
 from felis.state import State
 
-__all__ = ["Coroutine", "Either", "IO", "Lazy", "List", "Option", "ReverseState", "State"]
+__all__ = ["Coroutine", "Either", "Environment", "IO", "Lazy", "List", "Option", "ReverseState", "State"]
 
 __version__ = "0.dev0"
