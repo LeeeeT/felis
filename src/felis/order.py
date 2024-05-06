e6f3a0daca79eb63647f2dfd20ecf09a6f2f8c5e
LeeeeT <me@leeeet.dev>
2024-05-06 10:10:36 -0400

    Replace `ordering.Ordering` with `Ordering`

--- src/felis/order.py
+++ src/felis/order.py
@@ -4,12 +4,13 @@ from typing import Self
 
 from felis import ordering
 from felis.currying import curry
+from felis.ordering import Ordering
 from felis.typing import SupportsRichComparison
 
 __all__ = ["Order", "worse", "not_worse", "better", "not_better", "reverse", "neutral", "add", "map", "dunder", "rich_comparison"]
 
 
-type Order[T] = Callable[[T], Callable[[T], ordering.Ordering]]
+type Order[T] = Callable[[T], Callable[[T], Ordering]]
 
 
 @curry
@@ -38,31 +39,31 @@ def not_better[T](first: T, second: T, order: Order[T]) -> bool:
 
 @curry
 @curry
-def reverse[T](first: T, second: T, order: Order[T]) -> ordering.Ordering:
+def reverse[T](first: T, second: T, order: Order[T]) -> Ordering:
     return ordering.reverse(order(second)(first))
 
 
 @curry
-def neutral[T](first: T, second: T) -> ordering.Ordering:
+def neutral[T](first: T, second: T) -> Ordering:
     return ordering.neutral
 
 
 @curry
 @curry
 @curry
-def add[T](first: T, second: T, order_first: Order[T], order_second: Order[T]) -> ordering.Ordering:
+def add[T](first: T, second: T, order_first: Order[T], order_second: Order[T]) -> Ordering:
     return ordering.add(order_second(second)(first))(order_first(second)(first))
 
 
 @curry
 @curry
 @curry
-def map[From, To](first: To, second: To, order: Order[From], function: Callable[[To], From]) -> ordering.Ordering:
+def map[From, To](first: To, second: To, order: Order[From], function: Callable[[To], From]) -> Ordering:
     return order(function(second))(function(first))
 
 
 @curry
-def dunder[T: SupportsRichComparison](first: T, second: T) -> ordering.Ordering:
+def dunder[T: SupportsRichComparison](first: T, second: T) -> Ordering:
     if first > second:
         return ordering.Better()
     if first < second:
