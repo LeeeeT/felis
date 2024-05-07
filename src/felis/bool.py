97b45975936496ad100022beb71a5d2551ef1c7b
LeeeeT <me@leeeet.dev>
2024-05-07 16:38:28 -0400

    Add `felis.bool`

---
+++ src/felis/bool.py
@@ -0,0 +1,27 @@
+from felis.currying import curry
+
+__all__ = ["both", "either", "negate", "same", "different"]
+
+
+@curry
+def both(first: bool, second: bool) -> bool:
+    return first and second
+
+
+@curry
+def either(first: bool, second: bool) -> bool:
+    return first or second
+
+
+def negate(value: bool) -> bool:
+    return not value
+
+
+@curry
+def same(first: bool, second: bool) -> bool:
+    return first is second
+
+
+@curry
+def different(first: bool, second: bool) -> bool:
+    return first is not second
