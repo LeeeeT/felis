e05273a526d9e2813ee45316a0b5deefbccf1678
LeeeeT <me@LeeeeT.dev>
2024-06-04 14:47:45 -0800

    Encode `felis.Option` as ADT

--- src/felis/option.py
+++ src/felis/option.py
@@ -5,10 +5,15 @@ import felis.identity
 from felis import monad
 from felis.currying import curry
 
-__all__ = ["Option", "Some", "identity", "map", "inject", "join", "bind", "compose", "then"]
+__all__ = ["Option", "Nothing", "Some", "identity", "map", "inject", "join", "bind", "compose", "then"]
 
 
-type Option[T] = None | Some[T]
+type Option[T] = Nothing | Some[T]
+
+
+@dataclass(frozen=True)
+class Nothing:
+    pass
 
 
 @dataclass(frozen=True)
@@ -22,8 +27,8 @@ identity = Some
 @curry
 def map[From, To](option_value: Option[From], function: Callable[[From], To]) -> Option[To]:
     match option_value:
-        case None:
-            return None
+        case Nothing():
+            return Nothing()
         case Some(value):
             return Some(function(value))
 
@@ -31,8 +36,8 @@ def map[From, To](option_value: Option[From], function: Callable[[From], To]) ->
 @curry
 def inject[T, MOptionT](option_m_option_value: Option[MOptionT], m_identity: Callable[[Option[T]], MOptionT]) -> MOptionT:
     match option_m_option_value:
-        case None:
-            return m_identity(None)
+        case Nothing():
+            return m_identity(Nothing())
         case Some(m_option_value):
             return m_option_value
 
