a614cee1d55bfd5c4d7a35c10ec1b51348db7dd0
LeeeeT <me@LeeeeT.dev>
2024-06-11 05:35:34 -0800

    Add boolean and comparison predicates to `felis.predicate`

--- src/felis/predicate.py
+++ src/felis/predicate.py
@@ -2,8 +2,25 @@ from collections.abc import Callable
 
 import felis.bool
 from felis import function, identity
+from felis.currying import curry
+from felis.typing import SupportsRichComparison
 
-__all__ = ["Predicate", "both", "either", "negate", "same", "different"]
+__all__ = [
+    "Predicate",
+    "both",
+    "either",
+    "negate",
+    "same",
+    "different",
+    "truthy",
+    "falsey",
+    "less",
+    "less_or_equal",
+    "equal",
+    "not_equal",
+    "greater_or_equal",
+    "greater",
+]
 
 
 type Predicate[T] = Callable[[T], bool]
@@ -22,3 +39,40 @@ same = function.add(felis.bool.same)
 
 
 different = function.add(felis.bool.different)
+
+
+def truthy(value: object) -> bool:
+    return bool(value)
+
+
+falsey = negate(truthy)
+
+
+@curry
+def less[T: SupportsRichComparison](first: T, second: T) -> bool:
+    return first < second
+
+
+@curry
+def less_or_equal[T: SupportsRichComparison](first: T, second: T) -> bool:
+    return first <= second
+
+
+@curry
+def equal[T: SupportsRichComparison](first: T, second: T) -> bool:
+    return first == second
+
+
+@curry
+def not_equal[T: SupportsRichComparison](first: T, second: T) -> bool:
+    return first != second
+
+
+@curry
+def greater_or_equal[T: SupportsRichComparison](first: T, second: T) -> bool:
+    return first >= second
+
+
+@curry
+def greater[T: SupportsRichComparison](first: T, second: T) -> bool:
+    return first > second
