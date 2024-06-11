812c9e9b60cf7e7a2aa1af16e36cf1fb29f486be
LeeeeT <me@LeeeeT.dev>
2024-06-11 03:26:01 -0800

    Remove `felis.list.fold_left`, add `felis.list.neutral`, `felis.list.add`, `felis.list.fold`

--- src/felis/list.py
+++ src/felis/list.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+from typing import Protocol, cast
 
 import felis.order
 from felis import monad
@@ -6,7 +7,15 @@ from felis.currying import curry
 from felis.order import Order
 from felis.predicate import Predicate
 
-__all__ = ["identity", "map", "join", "bind", "compose", "then", "fold_left", "filter", "sort"]
+__all__ = ["neutral", "add", "identity", "fold", "map", "join", "bind", "compose", "then", "filter", "sort"]
+
+
+neutral = []
+
+
+@curry
+def add[T](augend: list[T], addend: list[T]) -> list[T]:
+    return augend + addend
 
 
 def identity[T](value: T) -> list[T]:
@@ -14,12 +23,27 @@ def identity[T](value: T) -> list[T]:
 
 
 @curry
+@curry
+def fold[T](list: list[T], add: Callable[[T], Callable[[T], T]], neutral: T) -> T:
+    match list:
+        case [head, *tail]:
+            return add(fold(neutral)(add)(tail))(head)
+        case _:
+            return neutral
+
+
+@curry
 def map[From, To](list_value: list[From], function: Callable[[From], To]) -> list[To]:
     return [function(value) for value in list_value]
 
 
-def join[T](list_list_value: list[list[T]]) -> list[T]:
-    return [value for list_value in list_list_value for value in list_value]
+# TODO: remove this
+class Join(Protocol):
+    def __call__[T](self, list_list_value: list[list[T]], /) -> list[T]: ...
+
+
+# TODO: remove the cast
+join = cast(Join, fold(neutral)(add))  # type: ignore
 
 
 bind = monad.bind(map)(join)
@@ -29,15 +53,6 @@ compose = monad.compose(bind)
 
 
 then = monad.then(bind)
-
-
-@curry
-@curry
-def fold_left[T](list: list[T], empty: T, add: Callable[[T], Callable[[T], T]]) -> T:
-    result = empty
-    for value in list:
-        result = add(value)(result)
-    return result
 
 
 @curry
