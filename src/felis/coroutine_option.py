e845316be5fe36553f4c28c4025b62c15afd760b
LeeeeT <me@LeeeeT.dev>
2024-06-19 17:54:46 -0800

    Add `felis.coroutine_option.apply` and `felis.coroutine_option.lift2`

--- src/felis/coroutine_option.py
+++ src/felis/coroutine_option.py
@@ -1,13 +1,19 @@
 import felis.identity
 from felis import applicative, coroutine, monad, option
 
-__all__ = ["map", "identity", "when", "join", "bind", "compose", "then"]
+__all__ = ["map", "identity", "apply", "lift2", "when", "join", "bind", "compose", "then"]
 
 
 map = felis.identity.compose(coroutine.map)(option.map)
 
 
 identity = felis.identity.compose(coroutine.identity)(option.identity)
+
+
+apply = coroutine.lift2(option.apply)
+
+
+lift2 = applicative.lift2(map)(apply)
 
 
 when = applicative.when(identity)
